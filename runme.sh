5fd03c1d514c5881593d2a174c456edf66d4085f
Eric Hanchrow <eric.hanchrow@gmail.com>
2023-06-14 08:34:57 -0700

    Add runme.sh

---
+++ runme.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+here="$(cd "$(dirname "$0")" && pwd)"
+set -x
+
+cd "${here}"
+
+if ! python3 -c 'import pipenv'
+then
+    echo "You don't have \`pipenv\`, so I'll install it for you"
+    python3 -m pip install --user --no-input pipenv
+fi
+
+if ! [ -x $here/.venv/bin/python3 ]
+then
+   python3 -m pipenv install
+fi
+
+./.venv/bin/python3 app/subway_data.py
